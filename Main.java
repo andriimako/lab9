a8b0c570d80932dd698437258590ba7441169115
s17386 <61780276+s17386@users.noreply.github.com>
2020-05-13 21:17:13 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[]args){
+        Car lol = new Car(8);
+        lol.start();
+        lol.stop();
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle {
+    public void start(){
+        System.out.println("ENGINE STARTED");
+    }
+    public void stop(){
+        System.out.println("ENGINE STOPPED");
+    }
+}
